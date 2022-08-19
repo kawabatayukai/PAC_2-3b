0914d14a88e6ca284d19b64d1f459c5290a5c279
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2022-08-19 16:53:42 +0900

    マップの1マスを 20→30px にしました
    マップチップの調整をしました
    1マス30×30pxです
    
    上下の隙間はなんとかします

--- Game_PACMAN/Game_PACMAN/Info.h
+++ Game_PACMAN/Game_PACMAN/Info.h
@@ -3,11 +3,11 @@
 #define SCREEN_WIDTH  1280 //スクリーン幅
 #define SCREEN_HEIGHT  720 //スクリーン高さ
 
-#define MAP_WIDTH  31    //Map X方向のチップ数
-#define MAP_HEIGHT  33   //Map Y方向のチップ数
-#define MAP_SIZE  20     //Map 一つのチップのサイズ (20 * 20)
+#define MAP_WIDTH   21     //Map X方向のチップ数               MAP_SIZE　20　→　31
+#define MAP_HEIGHT  23     //Map Y方向のチップ数                             →  33
+#define MAP_SIZE    30     //Map 一つのチップのサイズ
 
-//描画開始位置(スクリーンサイズ - ステージ全体の幅/高さ / ２)
-#define DRAW_POINT_X  330  //(SCREEN_WIDTH - (MAP_WIDTH * MAP_SIZE)) / 2   = 330
-#define DRAW_POINT_Y  33   //(SCREEN_HEIGHT - (MAP_HEIGHT * MAP_SIZE)) / 2  = 30
+//描画開始位置(スクリーンサイズ - ステージ全体の幅or高さ / ２)
+#define DRAW_POINT_X  (SCREEN_WIDTH - (MAP_WIDTH * MAP_SIZE)) / 2  
+#define DRAW_POINT_Y  (SCREEN_HEIGHT - (MAP_HEIGHT * MAP_SIZE)) / 2 
 
