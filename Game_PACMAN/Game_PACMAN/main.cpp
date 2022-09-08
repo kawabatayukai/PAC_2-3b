8dc55de8855ce1a05bfff3df2b5fa4089d92a1bd
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2022-09-08 09:06:38 +0900

    switch文による画面遷移
    入力取得用の変数なども追加してます

--- Game_PACMAN/Game_PACMAN/main.cpp
+++ Game_PACMAN/Game_PACMAN/main.cpp
@@ -2,6 +2,31 @@
 #include "Stage.h"
 #include "Image.h"
 
+//ゲームステート列挙体
+enum class GAME_STATE
+{
+	DRAW_TITLE = 0,     //タイトル
+	GAME_INIT,          //初期処理
+	GAME_MAIN,          //メイン
+	GAME_CLEAR,         //ゲームクリア
+	GAME_OVER,          //ゲームオーバー
+	GAME_END = 99       //ゲーム終了
+};
+
+
+//変数
+GAME_STATE g_GameState;  //ゲームステート
+int g_OldKey;            // 前回の入力キー 
+int g_NowKey;            // 今回の入力キー 
+int g_KeyFlg;            // 入力キー情報  
+
+//関数
+void DrawGameTitle();    //タイトル描画
+void GameInit();         //初期処理
+void GameMain();         //メイン
+void DrawGameClear();    //ゲームクリア描画
+void DrawGameOver();     //ゲームオーバー描画
+
 //メイン
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -11,7 +36,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if (DxLib_Init() == -1) return -1;     //DXライブラリの初期化処理
 	SetDrawScreen(DX_SCREEN_BACK);         //描画先画面を裏にする
 
-	if (image.LoadImages() == -1)return -1;
+	if (image.LoadImages() == -1)return -1;//画像読み込み
+
 
 
 	//ゲームループ              
@@ -19,12 +45,43 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	{
 		//画面の初期化
 		ClearDrawScreen();
+		//キー入力取得 
+		g_OldKey = g_NowKey;
+		g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1); 
+		g_KeyFlg = g_NowKey & ~g_OldKey;
 
-		//ステージの描画
-		//DrawGraph(0, 30, image.g_StageImage, TRUE);
-		stage.DrawMap();
-		
-	
+
+		switch (g_GameState)
+		{
+		case GAME_STATE::DRAW_TITLE:
+
+			DrawGameTitle();  //タイトル
+			break;
+
+		case GAME_STATE::GAME_INIT:
+
+			GameInit();       //初期処理
+			break;
+
+		case GAME_STATE::GAME_MAIN:
+
+			GameMain();       //ゲームメイン
+			break;
+
+		case GAME_STATE::GAME_CLEAR:
+
+			DrawGameClear();  //ゲームクリア描画
+			break;
+
+		case GAME_STATE::GAME_OVER:
+
+			DrawGameOver();   //ゲームオーバー描画
+			break;
+
+		default:
+			break;
+		}
+
 
 		//裏画面の内容を表画面に反映
 		ScreenFlip();
@@ -33,4 +90,41 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
 	return 0;				// ソフトの終了 
+}
+
+
+//タイトル描画
+void DrawGameTitle()
+{
+
+	//初期処理へ
+	g_GameState = GAME_STATE::GAME_INIT;
+}
+
+//初期処理
+void GameInit()
+{
+
+	//ゲームメインへ
+	g_GameState = GAME_STATE::GAME_MAIN;
+}
+
+//ゲームメイン
+void GameMain()
+{
+
+	//ステージの描画
+	stage.DrawMap();
+}
+
+//ゲームクリア
+void DrawGameClear()
+{
+
+}
+
+//ゲームオーバー
+void DrawGameOver()
+{
+
 }
