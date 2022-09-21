25b150c4abd4604c6a8197a3a6f7db9e1b3058fa
kawabatayukai <107020988+kawabatayukai@users.noreply.github.com>
2022-09-21 12:53:28 +0900

    音声クラス
    空っぽです

---
+++ Game_PACMAN/Game_PACMAN/sounds.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Sounds
+{
+public:
+	int LoadSounds(); //音声読み込み
+
+	void PlayPlayerMove();
+	void PlayPlayerEat();
+
+	void PlayIjikeEat();
+	void PlayEnemyEye();
+
+private:
+	int PlayerMove;  //プレイヤー移動
+	int PlayerEat;   //エサを食べる
+
+	int IjikeEat;    //イジケを捕食
+	int EnemyEye;    //目状態で巣に戻る
+};
